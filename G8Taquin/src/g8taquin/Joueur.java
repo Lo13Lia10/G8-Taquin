3945c75ad131a3d3bdb5dfd14fc7f959d38c8d8f
hazal <hazal@air-de-xez>
2020-10-06 17:25:33 +0200

    Creation classe joueur

--- G8Taquin/src/g8taquin/Joueur.java
+++ G8Taquin/src/g8taquin/Joueur.java
@@ -5,10 +5,29 @@
  */
 package g8taquin;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nci
  */
 public class Joueur {
+    private String pseudo="";  // represente le pseudo du joueur 
     
+     public void setPseudo(){
+  //jai repris notre ancienne fonction chez sahzamm 
+        Scanner sc = new Scanner(System.in);  //Initialisation du scanner
+        System.out.println("\nNouveau Joueur, entre ton pseudo : "); 
+        String ps = sc.nextLine();   //Saisie par le joueur
+        //Test si la saisie est valide
+        if ("".equals(ps.trim())){
+            System.out.println("Ton pseudo n'est pas valide, recommence.");
+            this.setPseudo(); 
+        } 
+        this.pseudo = ps;
+    }
+     public String getPseudo(){
+         //getteur du pseudo 
+     return pseudo;   
+} 
 }
