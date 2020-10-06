90cc641ce8fbc85ae4cf2098d90debec83a9451e
hazal <hazal@air-de-xez>
2020-10-06 17:21:40 +0200

    Creation classe Bloc

--- G8Taquin/src/g8taquin/Bloc.java
+++ G8Taquin/src/g8taquin/Bloc.java
@@ -10,5 +10,36 @@ package g8taquin;
  * @author nci
  */
 public class Bloc {
+   
+    int x; 
+    int y; 
+    int n; 
+    public Bloc (int x, int num,int y ){
+    //constructeur de la classe 
+        this.x=x; 
+        this.y=y; 
+        this.n=num; 
+        
+}
     
+    public void setCoordx (int x){
+        this.x=x; 
+    }
+    public void setCoordy(int y){
+        this.y=y; 
+    }
+
+    public void setNum(int num){
+        this.n=num; 
+    }
+    
+    public int  getCoordx (){
+        return x; 
+    }
+    public int  getCoordy (){
+        return y; 
+    }
+    public int  getNumBloc (){
+        return n; 
+    }
 }
